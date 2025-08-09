b25b2cb403fee910ad0c42c09e9e53232d30ca8f
SteveBob <88212395+Athurito@users.noreply.github.com>
2025-08-09 23:27:43 +0200

    19. GridSlots

---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/GridSlots/Inv_GridSlot.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "Inv_GridSlot.generated.h"
+
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_GridSlot : public UCommonUserWidget
+{
+	GENERATED_BODY()
+
+
+public:
+	void SetTileIndex(int32 Index) { TileIndex = Index; };
+	int32 GetTileIndex() const { return TileIndex; };
+
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_GridSlot;
+
+	int32 TileIndex;
+};
