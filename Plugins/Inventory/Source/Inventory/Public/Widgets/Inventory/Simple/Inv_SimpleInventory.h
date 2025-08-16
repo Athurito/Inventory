ab69a91453bb861a35d3edf15b2d08b40ea42d69
SteveBob <88212395+Athurito@users.noreply.github.com>
2025-08-16 13:17:24 +0200

    Refactor Simple Inventory to delegate grid functionality to Simple Inventory Grid widget.

--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Simple/Inv_SimpleInventory.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Simple/Inv_SimpleInventory.h
@@ -3,17 +3,10 @@
 #include "CoreMinimal.h"
 #include "Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
 #include "Types/Inv_GridTypes.h"
-#include "GameplayTagContainer.h"
 #include "Inv_SimpleInventory.generated.h"
 
-struct FGameplayTag;
-
-class UCanvasPanel;
-class UInv_GridSlot;
-class UInv_SlottedItem;
 class UInv_ItemComponent;
-class UInv_InventoryItem;
-class UInv_InventoryComponent;
+class UInv_SimpleInventoryGrid;
 
 UCLASS()
 class INVENTORY_API UInv_SimpleInventory : public UInv_InventoryBase
@@ -26,38 +19,8 @@ public:
 	// UInv_InventoryBase
 	virtual FInv_SlotAvailabilityResult HasRoomForItem(UInv_ItemComponent* ItemComponent) const override;
 
-protected:
-	UFUNCTION()
-	void OnItemAdded(UInv_InventoryItem* Item);
-
-	void ConstructGrid();
-	int32 FindFirstEmptySlotIndex() const;
-	int32 FindExistingStackIndex(const FGameplayTag& ItemType, int32 MaxStackSize) const;
-	void PlaceItemInSlot(UInv_InventoryItem* Item, int32 SlotIndex, int32 StackAmount);
-
 private:
-	// Layout
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	int32 Rows = 5;
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	int32 Columns = 6;
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	float TileSize = 64.f;
-
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	TSubclassOf<UInv_GridSlot> GridSlotClass;
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	TSubclassOf<UInv_SlottedItem> SlottedItemClass;
-
+	// The grid widget this window contains (bind in UMG BP)
 	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UCanvasPanel> CanvasPanel;
-
-	UPROPERTY()
-	TArray<TObjectPtr<UInv_GridSlot>> GridSlots;
-
-	UPROPERTY()
-	TMap<int32, TObjectPtr<UInv_SlottedItem>> SlottedItems;
-
-	UPROPERTY()
-	TWeakObjectPtr<UInv_InventoryComponent> InventoryComponent;
+	TObjectPtr<UInv_SimpleInventoryGrid> Grid_Simple;
 };
