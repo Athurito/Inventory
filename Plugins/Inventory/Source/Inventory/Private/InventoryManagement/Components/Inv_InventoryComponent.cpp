488a4b31d3f770a2ba2d746aca13f9dcf7c22be9
SteveBob <88212395+Athurito@users.noreply.github.com>
2025-08-23 17:32:06 +0200

    108. Server Drop Item

--- Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/Inv_InventoryComponent.cpp
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/Inv_InventoryComponent.cpp
@@ -102,6 +102,26 @@ void UInv_InventoryComponent::Server_AddStacksToItem_Implementation(UInv_ItemCom
 	}
 }
 
+void UInv_InventoryComponent::Server_DropItem_Implementation(UInv_InventoryItem* Item, int32 StackCount)
+{
+	const int32 NewStackCount = Item->GetTotalStackCount() - StackCount;
+	if (NewStackCount <= 0)
+	{
+		InventoryList.RemoveEntry(Item);
+	}
+	else
+	{
+		Item->SetTotalStackCount(NewStackCount);
+	}
+	SpawnDroppedItem(Item, StackCount);
+}
+
+void UInv_InventoryComponent::SpawnDroppedItem(UInv_InventoryItem* Item, int32 StackCount)
+{
+	//TODO Spawn
+}
+
+
 void UInv_InventoryComponent::ToggleInventoryMenu()
 {
 	if (bIsInventoryMenuOpen)
--- Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
@@ -31,9 +31,13 @@ public:
 	UFUNCTION(Server, Reliable)
 	void Server_AddStacksToItem(UInv_ItemComponent* ItemComponent, int32 StackCount, int32 Remainder);
 
-	void ToggleInventoryMenu();
+	UFUNCTION(Server, Reliable)
+	void Server_DropItem(UInv_InventoryItem* Item,  int32 StackCount);
 
+	void ToggleInventoryMenu();
 	void AddRepSubObject(UObject* SubObject);
+	void SpawnDroppedItem(UInv_InventoryItem* Item, int32 StackCount);
+	
 
 	FInventoryItemChange OnItemAdded;
 	FInventoryItemChange OnItemRemoved;
