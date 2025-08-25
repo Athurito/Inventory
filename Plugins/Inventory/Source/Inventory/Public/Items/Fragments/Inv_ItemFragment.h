83a600ac0caf8650155ee55685a2adac97a6fc64
SteveBob <88212395+Athurito@users.noreply.github.com>
2025-08-25 22:21:35 +0200

    128. Consume Modifiers

--- Plugins/Inventory/Source/Inventory/Public/Items/Fragments/Inv_ItemFragment.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Fragments/Inv_ItemFragment.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
+#include "StructUtils/InstancedStruct.h"
 
 #include "Inv_ItemFragment.generated.h"
 
@@ -106,6 +107,7 @@ struct FInv_LabeledNumberFragment : public FInv_InventoryItemFragment
 
 	virtual void Assimilate(UInv_CompositeBase* Composite) const override;
 	virtual void Manifest() override;
+	float GetValue() const { return Value; }
 
 	// When manifesting for the first time, this fragment will randomize. However, onee equipped
 	// and dropped, an item should retain the same value, so randomization should not occur.
@@ -159,33 +161,41 @@ private:
 	
 };
 
+// Consume Fragments
+
 USTRUCT(BlueprintType)
-struct FInv_ConsumableFragment : public FInv_ItemFragment
+struct FInv_ConsumeModifier: public FInv_LabeledNumberFragment
 {
 	GENERATED_BODY()
-
 	virtual void OnConsume(APlayerController* PC) {}
-	
 };
 
 USTRUCT(BlueprintType)
-struct FInv_HealthPotionFragment : public FInv_ConsumableFragment
+struct FInv_ConsumableFragment : public FInv_InventoryItemFragment
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	float HealAmount = 20.f;
+	virtual void OnConsume(APlayerController* PC);
+	virtual void Assimilate(UInv_CompositeBase* Composite) const override;
+	virtual void Manifest() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Inventory", meta = (ExcludeBaseStruct))
+	TArray<TInstancedStruct<FInv_ConsumeModifier>> ConsumeModifiers;
+};
+
+USTRUCT(BlueprintType)
+struct FInv_HealthPotionFragment : public FInv_ConsumeModifier
+{
+	GENERATED_BODY()
+
 	virtual void OnConsume(APlayerController* PC) override;
-	
 };
 
 USTRUCT(BlueprintType)
-struct FInv_ManaPotionFragment : public FInv_ConsumableFragment
+struct FInv_ManaPotionFragment : public FInv_ConsumeModifier
 {
 	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere, Category = "Inventory")
-	float ManaAmount = 20.f;
 
 	virtual void OnConsume(APlayerController* PC) override;
 };
